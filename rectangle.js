10bf919035a300896cb1c75e937d26a4ac11c149
wujinya <2601519403@qq.com>
2020-02-26 09:58:48 +0800

    rectangle.js

--- rectangle.js
+++ rectangle.js
@@ -11,21 +11,12 @@ $(function(){
 
     var p = 2*(w+h),
         a = w*h;
-    
-   // var p1 = Math.round(p*Math.pow(10,1)) / Math.pow(10,1),
-    //    a2 = Math.round(a*Math.pow(10,1)) / Math.pow(10,1);
-    var p1,a1;
-    function roundFractional(p, 1) {
-      return p1 =  Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-      
-    }
-    function roundFractional(a, 1) {
-      return a1 =  Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-
-    }
-  
-    
-    $perimeter.val(p1);
-    $area.val(a1);
+     
+    $perimeter.val(roundFractional(p,2));
+    $area.val(roundFractional(a,2));
   });
 });
+
+function roundFractional(x, n) {
+    return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+}
