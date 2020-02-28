32edb8d9f213b3526fa433b640c2495b5d200bc2
wujinya <2601519403@qq.com>
2020-02-28 11:00:03 +0800

    rectangleV0.2

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    htmlhint:{
+      options:{
+        htmlhintrc:'./.htmlhintrc'        
+      },
+      src: '*.html'
+    },
+    csslint: {
+      options: {
+        csslintrc: './.csslintrc'
+      },
+      src: '*.css'
+    },
+    eslint: {
+      options: {
+        configFile: './.eslintrc.json'                
+      }, 
+      target: ['*.js']        
+    }
+  });
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-eslint');  
+  grunt.registerTask('default', ['htmlhint', 'csslint', 'eslint']);
+};
--- rectangle.js
+++ rectangle.js
@@ -1,22 +1,20 @@
+/* global $*/
 $(function(){
   var $width = $('#width'),
       $height = $('#height'),
       $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
       $area = $('#area');
-
   $btnCal.click(function(){
     var w = Number($width.val()),
         h = Number($height.val());
-
     var p = 2*(w+h),
         a = w*h;
-     
     $perimeter.val(roundFractional(p,2));
     $area.val(roundFractional(a,2));
   });
 });
 
 function roundFractional(x, n) {
-    return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+  return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
 }
